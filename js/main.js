ab94a8a8e17a3e4777776105372a3e4a03fec5a9
tayoshi-tokisada <mail.by.blacksheep@gmail.com>
2024-05-17 23:44:48 +0900

    change トップ画面作成

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+  // カルーセル
+  $(".topCarousel").slick({
+    autoplay: true,
+    dots: false,
+    infinite: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    slidesToShow: 1,
+  });
+})
