259fcfde883decf0ceab255dea0dd9af7565569c
tayoshi-tokisada <mail.by.blacksheep@gmail.com>
2024-05-28 23:07:08 +0900

    change index.html

--- js/main.js
+++ js/main.js
@@ -8,4 +8,12 @@ $(function(){
     arrows: false,
     slidesToShow: 1,
   });
+  $(".bottomCarousel").slick({
+    autoplay: true,
+    dots: false,
+    infinite: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    slidesToShow: 1,
+  });
 })
