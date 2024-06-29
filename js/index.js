882f083c9db61d6314fda0799cc38b53c8260865
tayoshi-tokisada <mail.by.blacksheep@gmail.com>
2024-06-29 18:03:56 +0900

    change index.html topCarouselのレスポンシブ対応

--- js/index.js
+++ js/index.js
@@ -16,4 +16,20 @@ $(function(){
     arrows: false,
     slidesToShow: 1,
   });
+  $(".SP-topCarousel").slick({
+    autoplay: true,
+    dots: false,
+    infinite: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    slidesToShow: 1,
+  });
+  $(".SP-bottomCarousel").slick({
+    autoplay: true,
+    dots: false,
+    infinite: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    slidesToShow: 1,
+  });
 })
